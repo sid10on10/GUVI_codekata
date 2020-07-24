635eeb118c49ed24b81ce001870dacbe58395240
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-24 09:30:00 +0530

    AB problem 6

---
+++ absolute_beginner/problem6.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+//Your code goes here … replace this line with your code logic 
+const number = +userInput[0]
+const circum = 2*(22/7)*number
+console.log(circum.toFixed(2))
+
+//end-here
+});
