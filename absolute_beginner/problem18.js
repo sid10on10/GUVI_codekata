e93fa4c3b9e171ee0c187b3aceb7a71e2216eb16
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-24 09:36:07 +0530

    AB problem 18

---
+++ absolute_beginner/problem18.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+//Your code goes here … replace this line with your code logic 
+const mynumbers = userInput.map(val => +val)
+mynumbers.sort()
+console.log(mynumbers[2])
+//end-here
+});
