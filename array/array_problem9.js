4519282eb3b76e23443941d014ded6c46efae447
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-25 14:36:48 +0530

    array problem 9

---
+++ array/array_problem9.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+//Your code goes here … replace this line with your code logic
+let n = +userInput[0]
+let numbers = userInput[1].split(" ").map(val => +val)
+let minimumno = numbers[0];
+
+for (i of numbers){
+    if (i < minimumno){
+        minimumno = i
+    }
+}
+console.log(minimumno)
+
+//end-here
+});
