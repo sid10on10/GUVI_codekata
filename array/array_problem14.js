497d080a450c37a974c0587ee98f328914e9a2ab
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-27 15:34:42 +0530

    array problem 14

---
+++ array/array_problem14.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+//Your code goes here … replace this line with your code logic 
+var arr = userInput[0].split("")
+for (let index=0;index<arr.length;index++){
+    if (arr[index]===arr[index+1]){
+        arr.splice(index,2)
+    }
+}
+console.log(arr.join(""))
+//end-here
+});
