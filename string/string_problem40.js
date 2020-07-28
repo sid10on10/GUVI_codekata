16b91fbddae4067238310f85b1239394c5868948
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-28 18:53:12 +0530

    string_problem40

---
+++ string/string_problem40.js
@@ -0,0 +1,26 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let str = userInput[0]
+if(isNaN(str)){
+    console.log("no")
+}else{
+    console.log("yes")
+}
+
+
+  //end-here
+});
