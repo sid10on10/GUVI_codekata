418607d66d34902b967a1c3c940c6afde9aed678
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-28 13:46:34 +0530

    string problem 35

---
+++ string/string_problem35.js
@@ -0,0 +1,22 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let strarr = userInput[0].split(" ")
+strarr.sort()
+console.log(strarr.join(" "))
+
+  //end-here
+});
