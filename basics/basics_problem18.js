0107d6aafb52b200e86481358ded76c34d539ba8
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-31 22:28:23 +0530

    basic 18

---
+++ basics/basics_problem18.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+//Your code goes here … replace this line with your code logic 
+let n = +userInput[0]
+let numbers = userInput[1].split(" ").map(val=>+val)
+let ans = numbers[0]
+
+for(i of numbers){
+    ans = i|ans
+}
+console.log(ans)
+
+
+
+//end-here
+});
