0e1e0c995cf5c37fe85a92c4bdc6080f938858d3
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-31 22:29:15 +0530

    basic 24

---
+++ basics/basics_problem24.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+//Your code goes here … replace this line with your code logic 
+let mystr = userInput[0].split(" ")
+mystr.reverse()
+console.log(mystr.join(" "))
+
+
+
+
+//end-here
+});
