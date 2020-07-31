0c79814b397293b5afa8da7d2c429fbfcb049073
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-31 23:22:07 +0530

    string index of 1 from right

---
+++ string/index_of_1_from_right.js
@@ -0,0 +1,21 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let bin = (+userInput[0]).toString(2)
+console.log(String(bin).split("").reverse().indexOf("1")+1)
+
+  //end-here
+});
