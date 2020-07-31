9c839054238f83d57a9ed991a69ea0ded1e3761c
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-31 22:29:43 +0530

    basic 28

---
+++ basics/basics_problem28.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+//Your code goes here … replace this line with your code logic 
+let n = +userInput[0]
+console.log(~n)
+
+
+
+//end-here
+});
