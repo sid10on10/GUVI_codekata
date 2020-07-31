4386a467fc9f3c046ef7f45f67a4523dff6aa288
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-07-31 17:15:22 +0530

    string bin to hexa

---
+++ string/binary_to_hexa.js
@@ -0,0 +1,22 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let n = userInput[0]
+var hexa = parseInt(n, 2).toString(16).toUpperCase();
+console.log(hexa)
+
+  //end-here
+});
