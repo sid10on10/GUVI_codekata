ccab4e6f3e7149f9b83ae267b84dbe876b2f1f3b
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-01 19:13:39 +0530

    string max_no_digits

---
+++ string/max_no_digits.js
@@ -0,0 +1,24 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let narr = userInput[0].split("").map(val=>+val)
+
+narr.sort((a,b)=>b-a)
+console.log(narr.join(""))
+
+
+  //end-here
+});
