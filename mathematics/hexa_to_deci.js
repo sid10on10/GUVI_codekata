f56576137df667e20f939c9dbe33fc569bfc8f79
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-03 22:08:33 +0530

    Maths problem

---
+++ mathematics/hexa_to_deci.js
@@ -0,0 +1,22 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let hexa = userInput[0]
+let deci = parseInt(hexa,16)
+console.log(deci)
+
+  //end-here
+});
