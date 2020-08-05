114f72e915c32d33a8f2b30683b81e2ad25f0570
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-05 20:32:30 +0530

    maths problem

---
+++ mathematics/sum_first_k.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+//start-here
+//Your code goes here … replace this line with your code logic 
+let n = +userInput[0]
+let sum = n*(n+1)/2
+console.log(sum)
+
+
+
+//end-here
+});
