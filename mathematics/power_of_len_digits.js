35fc1efe5d1e2a87afbb0877cb2fedd8b81ee5ba
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-05 20:29:58 +0530

    maths problem

---
+++ mathematics/power_of_len_digits.js
@@ -0,0 +1,28 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let digarr = userInput[0].split("").map(val=>+val)
+let sum = 0
+
+for(let i=0;i<digarr.length;i++){
+    sum += digarr[i]**i
+}
+console.log(sum)
+
+
+
+  //end-here
+});
