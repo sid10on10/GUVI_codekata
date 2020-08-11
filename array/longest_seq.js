e42d6383ca1148f3be0623a78d79f20687738e6b
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-11 22:43:30 +0530

    array problem

---
+++ array/longest_seq.js
@@ -0,0 +1,36 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let n = +userInput[0]
+let nums = userInput[1].split(" ").map(val=>+val)
+
+let max = []
+
+for(let i=0;i<n;i++){
+    let count = 0;
+    while(nums[i]==nums[i+1]){
+        count++;
+        i++;
+    }
+    max.push(count)
+}
+if(Math.max(...max)===0){
+    console.log(-1)
+}else{
+console.log(Math.max(...max)+1)
+} 
+  //end-here
+});
