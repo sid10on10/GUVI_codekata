943be1abeb5de96d71fff7b90f88ad6592431050
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-11 22:43:48 +0530

    array problem

---
+++ array/del_k_last.js
@@ -0,0 +1,24 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let [n,k] = userInput[0].split(" ").map(val=>+val)
+let nums = userInput[1].split(" ").map(val=>+val)
+
+nums.splice(n-k,k)
+console.log(nums.join(" "))
+
+  //end-here
+});
