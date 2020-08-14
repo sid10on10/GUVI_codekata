df16d33a5ac11c1b342e30c5d48bd6ce017c3e01
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-14 16:49:34 +0530

    array problem

---
+++ array/bin_to_octal.js
@@ -0,0 +1,21 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let n = userInput[0]
+let result = parseInt(n,2).toString(8)
+console.log(result)
+  //end-here
+});
