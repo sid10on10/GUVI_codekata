29b029fbab174b625a1d67ec5388420dba6db823
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-15 19:54:43 +0530

    array problem

---
+++ array/max_of_consecutive.js
@@ -0,0 +1,27 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let n = +userInput[0]
+let nums = userInput[1].split(" ").map(val=>+val)
+let outarr = []
+
+for(let i=0;i<nums.length-1;i++){
+    outarr.push(Math.max(nums[i],nums[i+1]))
+}
+console.log(outarr.join(" "))
+
+  //end-here
+});
