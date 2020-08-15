f7bb81a2675847f18bc163598bdb8b12ccbf2190
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-15 19:54:19 +0530

    array problem

---
+++ array/max_diff.js
@@ -0,0 +1,24 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let n = +userInput[0]
+let nums = userInput[1].split(" ").map(val=>+val)
+nums.sort((a,b)=>a-b)
+
+console.log(nums[n-1]-nums[0])
+
+  //end-here
+});
