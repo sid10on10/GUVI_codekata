dc60e94719e1640aba22a0c9626940c94f409183
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-15 19:55:34 +0530

    array problem

---
+++ array/max_sum_of_adja_cons.js
@@ -0,0 +1,22 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let n = +userInput[0]
+let nums = userInput[1].split(" ").map(val=>+val)
+
+console.log((nums.sort((a,b)=>b-a)[0]+nums.sort((a,b)=>b-a)[1])*2)
+  //end-here
+});
