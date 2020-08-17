d725b5dafd107c2dfacdaeab03066cf0022cddd5
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-17 22:51:34 +0530

    maths problem

--- mathematics/k_rotation.js
+++ mathematics/k_rotation.js
@@ -14,16 +14,18 @@ inp.on("line", (data) => {
 inp.on("close", () => {
   //start-here
   //Your code goes here … replace the below line with your code logic 
-let [name,marks1,marks2,marks3] = userInput[0].split("#")
-let [name1,marks4,marks5,marks6] = userInput[1].split("#")
+let [n,k] = userInput[0].split(" ").map(val=>+val)
+let nums = userInput[1].split(" ").map(val=>+val)
 
-let sum1 = (+marks1)+(+marks2)+(+marks3)
-let sum2 = (+marks4)+(+marks5)+(+marks6)
-
-if(sum1>sum2){
-    console.log(name)
+if(k<n){
+    let cutarr = nums.splice(0,k)
+    let outarr = nums.concat(cutarr)
+    console.log(outarr.join(" ")) 
 }else{
-    console.log(name1)   
+    k = k%n
+    let cutarr = nums.splice(0,k)
+    let outarr = nums.concat(cutarr)
+    console.log(outarr.join(" ")) 
 }
 
   //end-here
