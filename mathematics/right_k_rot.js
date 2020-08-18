292a8ff205cc354e7d503a5525b81c6680ba0840
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-18 22:21:13 +0530

    maths problem

---
+++ mathematics/right_k_rot.js
@@ -0,0 +1,32 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let [n,k] = userInput[0].split(" ").map(val=>+val)
+let nums = userInput[1].split(" ").map(val=>+val)
+
+if(k<n){
+    let cutarr = nums.splice(n-k,k)
+    let outarr = cutarr.concat(nums)
+    console.log(outarr.join(" ")) 
+}else{
+    k = k%n
+    let cutarr = nums.splice(n-k,k)
+    let outarr = cutarr.concat(nums)
+    console.log(outarr.join(" ")) 
+}
+
+  //end-here
+});
