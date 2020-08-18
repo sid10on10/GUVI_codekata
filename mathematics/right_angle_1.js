9f98cdcdc89776411d58dcf9984b080bf296665d
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-08-18 22:21:02 +0530

    maths problem

---
+++ mathematics/right_angle_1.js
@@ -0,0 +1,23 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let n = +userInput[0]
+for(let i=n;i>0;i--){
+    console.log("1 ".repeat(i-1)+"1")
+}
+
+  //end-here
+});
