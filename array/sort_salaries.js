602a17cb019f607a59d7afe1852e4dac731e4d0b
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-09-13 23:20:41 +0530

    codekata problem

--- array/sort_salaries.js
+++ array/sort_salaries.js
@@ -15,9 +15,15 @@ inp.on("close", () => {
   //start-here
   //Your code goes here … replace the below line with your code logic 
 let n = +userInput[0]
-let numbers = userInput[1].split(" ").map(val=>+val)
-numbers.sort((a,b)=>a-b)
-console.log(numbers.join(" "))
+let data = userInput[1].split(" ")
+let dataarr = []
 
+for(let i=0;i<2*n;i+=2){
+    dataarr.push([data[i],parseInt(data[i+1])])
+}
+dataarr.sort((a,b)=>a[1]-b[1])
+for(i of dataarr){
+    console.log(i[0])
+}
   //end-here
 });
